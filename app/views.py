7693ced4e4fb24cf5ae2a840d69696087371573e
kumarkalyan07570 <kumarkalyan07570@gmail.com>
2023-11-28 23:45:17 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def kalyan(requrest):
+    return render(requrest,'kalyan.html')
+
+def sam(requrest):
+    return render(requrest,'sam.html')
+
+def kalyan2(requrest):
+    return render(requrest,'kalyan.html')
+                  
